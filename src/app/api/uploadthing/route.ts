c0e03cc15d1f1dfeaf864c8aee78383607484382
Alex Tomjack <amt1309@gmail.com>
2024-09-16 19:43:00 -0500

    Reverted uploadthing route to create the get and post handlers separately

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,11 +3,11 @@ import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
 // Export routes for Next App Router
-const handler = createRouteHandler({
+const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 
   // Apply an (optional) custom config:
   // config: { ... },
 });
 
-export { handler as GET, handler as POST };
+export { GET, POST };
