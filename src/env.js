75e923f35a667e5190b26420b261241d80c07f6d
Alex Tomjack <amt1309@gmail.com>
2024-12-23 02:07:37 -0600

    Update env validation

--- src/env.js
+++ src/env.js
@@ -35,7 +35,7 @@ export const env = createEnv({
   runtimeEnv: {
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    AUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
     AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
