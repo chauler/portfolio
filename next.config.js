ce6a3a5c37f44f83c51bfe67801f18ecc07ac364
Alex Tomjack <amt1309@gmail.com>
2025-02-05 09:49:22 -0600

    Changes to reverse proxy

--- next.config.js
+++ next.config.js
@@ -29,12 +29,26 @@ const nextConfig = {
     remotePatterns: [{ protocol: "https", hostname: "utfs.io" }],
   },
   rewrites: async () => {
-    return [
-      {
-        source: "/game",
-        destination: "http://5.161.61.70:3000/", // Matched parameters can be used in the destination
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/game",
+          destination: "http://5.161.61.70:3000/", // Matched parameters can be used in the destination
+        },
+      ],
+      afterFiles: [
+        {
+          source: "/game",
+          destination: "http://5.161.61.70:3000/",
+        },
+      ],
+      fallback: [
+        {
+          source: "/game",
+          destination: "http://5.161.61.70:3000/",
+        },
+      ],
+    };
   },
 };
 
